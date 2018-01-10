f8050e4004afeb95fbffa7bd4466a56176b79011
Chris Neale <chris@komododigital.co.uk>
2018-01-10 10:14:06 +0000

    Switching to workers

---
+++ src/mosaic.worker.js
@@ -0,0 +1,10 @@
+import triangle from "./triangle";
+import square from "./square";
+import hexagon from "./hex";
+import circle from "./circle";
+
+export class Mosaic {
+  constructor() {
+    // Worker for building mosaics
+  }
+}
