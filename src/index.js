706f2034540b15fb4df29da306f9cdd3a2c9b5dc
Chris Neale <chris@komododigital.co.uk>
2018-01-18 08:40:03 +0000

    Added listeners to form fields

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import MosaicWorker from "./mosaic.worker.js";
 import "./patterns.css";
 
 let progress = 0;
+let imgCache;
 
 let mosaicWorker = new MosaicWorker();
 mosaicWorker.addEventListener("message", e => {
@@ -23,17 +24,21 @@ mosaicWorker.addEventListener("message", e => {
   }
 });
 
-var getScaledImageData = function(imgSize, img) {
+var getScaledImageData = function(imgSize) {
   var c = document.createElement("canvas");
-  var aspect = img.height / img.width;
+  var aspect = imgCache.height / imgCache.width;
   c.width = imgSize;
   c.height = Math.floor(imgSize * aspect);
 
   var ctx = c.getContext("2d");
 
-  ctx.drawImage(img, 1, 1, imgSize, Math.floor(imgSize * aspect));
+  ctx.drawImage(imgCache, 1, 1, imgSize, Math.floor(imgSize * aspect));
 
-  return ctx.getImageData(0, 0, c.width, c.height).data;
+  return {
+    data: ctx.getImageData(0, 0, c.width, c.height).data,
+    width: c.width,
+    height: c.height
+  };
 };
 
 var holder = document.body;
@@ -52,41 +57,44 @@ holder.ondrop = function(e) {
   var reader = new FileReader();
 
   reader.onload = function(event) {
-    let shapeSelect = document.getElementById("shape");
-    let shape = shapeSelect.options[shapeSelect.selectedIndex].value;
-    var cellSize = parseInt(document.getElementById("cellsize").value) || 5;
-    var imgSize = parseInt(document.getElementById("size").value) || 48;
-    var variance = parseInt(document.getElementById("variance").value) || 30;
-    var padding = parseInt(document.getElementById("gap").value) || 0;
-
     var img = document.createElement("img");
     img.src = event.target.result;
 
-    img.addEventListener("load", () => {
-      var data = getScaledImageData(imgSize, img);
-
-      if (window.Worker) {
-        document.body.classList.add("black");
-        mosaicWorker.postMessage({
-          type: "create",
-          shape,
-          imgSize,
-          cellSize,
-          padding,
-          aspect: img.height / img.width,
-          variance,
-          data,
-          img: img.src
-        });
-      } else {
-        document.body.innerHTML = "Sorry, you need web workers.";
-      }
+    img.addEventListener("load", function() {
+      imgCache = img;
+      createSVG();
     });
   };
 
   reader.readAsDataURL(file);
 
   return false;
+};
+
+let createSVG = function() {
+  let shapeSelect = document.getElementById("shape");
+  let shape = shapeSelect.options[shapeSelect.selectedIndex].value;
+  var cellSize = parseInt(document.getElementById("cellsize").value) || 5;
+  var imgSize = parseInt(document.getElementById("size").value) || 48;
+  var padding = parseInt(document.getElementById("gap").value) || 0;
+  var variance = parseInt(document.getElementById("variance").value) || 30;
+  var data = getScaledImageData(imgSize);
+
+  if (window.Worker) {
+    document.body.classList.add("black");
+    mosaicWorker.postMessage({
+      type: "create",
+      shape,
+      imgSize,
+      cellSize,
+      padding,
+      aspect: data.height / data.width,
+      variance,
+      data: data.data
+    });
+  } else {
+    document.body.innerHTML = "Sorry, you need web workers.";
+  }
 };
 
 var c = document.createElement("canvas");
@@ -102,7 +110,9 @@ linearGradient1.addColorStop(1, "rgb(  0, 0, 0)");
 ctx.fillStyle = linearGradient1;
 ctx.fillRect(0, 0, 128, 128);
 
-let data = ctx.getImageData(0, 0, c.width, c.height).data;
+imgCache = c;
+
+let data = getScaledImageData(128);
 
 mosaicWorker.postMessage({
   type: "create",
@@ -110,7 +120,14 @@ mosaicWorker.postMessage({
   imgSize: 128,
   cellSize: 6,
   padding: 3,
-  aspect: 600 / 800,
+  aspect: data.height / data.width,
   variance: 30,
-  data: data
+  data: data.data
 });
+
+document.getElementById("shape").addEventListener("change", createSVG);
+document.getElementById("cellsize").addEventListener("change", createSVG);
+document.getElementById("size").addEventListener("change", createSVG);
+document.getElementById("gap").addEventListener("change", createSVG);
+document.getElementById("variance").addEventListener("change", createSVG);
+document.getElementById("scaling").addEventListener("change", createSVG);
