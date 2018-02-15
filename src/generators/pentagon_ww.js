62fa418c52bbc8403629e7efffc5979a02da38fa
Chris Neale <chris@komododigital.co.uk>
2018-02-15 13:10:41 +0000

    Better pentagon layout

--- src/generators/pentagon_ww.js
+++ src/generators/pentagon_ww.js
@@ -8,27 +8,27 @@ export default class Hex extends base {
     this.h = this.cellSize / 1 + this.padding;
 
     let dist = [
-      this.cellSize,
-      this.cellSize,
-      this.cellSize,
-      this.cellSize,
-      this.cellSize
+      this.cellSize * 1,
+      this.cellSize * 1,
+      this.cellSize * 1,
+      this.cellSize * 1,
+      this.cellSize * (Math.sqrt(3) - 1)
     ];
-    let angles = [75, 75, 60, 75, 75];
+    let angles = [80, 80, 40, 80, 80];
     let ia = 0;
 
     let path = "M 0 0 ";
     for (var x = 0; x < 5; x++) {
       ia = ia + angles[x];
       let r = Math.PI / 180 * ia;
-      this.extents.x.push(this.round(Math.cos(r) * this.cellSize / 2));
-      this.extents.y.push(this.round(Math.sin(r) * this.cellSize / 2));
+      this.extents.x.push(this.round(Math.cos(r) * dist[x] / 2));
+      this.extents.y.push(this.round(Math.sin(r) * dist[x] / 2));
       path += "L ";
-      path += this.round(Math.cos(r) * this.cellSize / 2) + " ";
-      path += this.round(Math.sin(r) * this.cellSize / 2) + " ";
+      path += this.round(Math.cos(r) * dist[x] / 2) + " ";
+      path += this.round(Math.sin(r) * dist[x] / 2) + " ";
     }
     let r = Math.PI / 180 * angles[0];
-    let c = this.cellSize / 2;
+    let c = dist[0] / 2;
     path += "L ";
     path += this.round(Math.cos(r) * c) + " ";
     path += this.round(Math.sin(r) * c) + " ";
@@ -72,25 +72,25 @@ export default class Hex extends base {
           // bottom
           rotate = `rotate(270)`;
           // col = "rgb(255,0,0)";
-          y += this.mY / 2.5;
+          y += this.mY / 2.7;
           break;
         case 1:
           // left
           rotate = `rotate(180)`;
           // col = "rgb(255,255,255)";
-          x -= this.mX / 4;
+          x -= this.mX / 2.5;
           break;
         case 2:
           // right
           rotate = `rotate(0)`;
           // col = "rgb(255,255,0)";
-          x += this.mX / 4;
+          x += this.mX / 2.5;
           break;
         case 3:
           // top
           rotate = `rotate(90)`;
           // col = "rgb(255,0,255)";
-          y -= this.mY / 2.5;
+          y -= this.mY / 2.7;
           break;
       }
 
