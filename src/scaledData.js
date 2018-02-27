65ae05c4313befd971be4e3ee77b34bc9b12ee81
Chris Neale <chris@usablehq.com>
2018-02-27 19:24:17 +0000

    Update scaledData.js

--- src/scaledData.js
+++ src/scaledData.js
@@ -1,3 +1,12 @@
+/**
+ * Scale an image using a canvas
+ * @constructor
+ * @external document - A browser document instance
+ * @param {number} imgCache - cached image
+ * @param {number} imgSize - img size to scale to
+ * @param {number} dX - distortion factor in the x axis
+ * @param {number} dY - distortion factor in the y axis
+ */
 export function getScaledImageData(imgCache, imgSize, dX, dY) {
   const distX = dX || 1;
   const distY = dY || 1;
